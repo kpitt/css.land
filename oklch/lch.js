73bb262fc4446dfeb27243481e5f4c7c96fa3b0a
Kenny Pitt <kenny.pitt@gmail.com>
2022-04-01 11:20:15 -0400

    [oklch] Implement improved gamut mapping with local clipping
    See: https://www.w3.org/TR/css-color-4/#GM-chroma-local-MINDE

--- oklch/lch.js
+++ oklch/lch.js
@@ -1,5 +1,20 @@
 const supportsP3 = self.CSS && CSS.supports("color", "color(display-p3 0 1 0)");
 
+const sRGB_space = {
+	from_lch: OKLCH_to_sRGB,
+	to_lch: sRGB_to_OKLCH,
+}
+
+const P3_space = {
+	from_lch: OKLCH_to_P3,
+	to_lch: P3_to_OKLCH,
+}
+
+const r2020_space = {
+	from_lch: OKLCH_to_r2020,
+	to_lch: r2020_to_OKLCH,
+}
+
 function alpha_to_string(a = 100) {
 	return (a < 100? ` / ${a}%` : "");
 }
@@ -13,7 +28,7 @@ function LCH_to_r2020_string(l, c, h, a = 100) {
 
 function LCH_to_P3_string(l, c, h, a = 100, forceInGamut = false) {
 	if (forceInGamut) {
-		[l, c, h] = force_into_gamut(l, c, h, isLCH_within_P3);
+		[l, c, h] = force_into_gamut(P3_space, l, c, h);
 	}
 
 	return "color(display-p3 " + OKLCH_to_P3([+l / 100, +c, +h]).map(x => {
@@ -24,7 +39,7 @@ function LCH_to_P3_string(l, c, h, a = 100, forceInGamut = false) {
 
 function LCH_to_sRGB_string(l, c, h, a = 100, forceInGamut = false) {
 	if (forceInGamut) {
-		[l, c, h] = force_into_gamut(l, c, h, isLCH_within_sRGB);
+		[l, c, h] = force_into_gamut(sRGB_space, l, c, h);
 	}
 
 	return "rgb(" + OKLCH_to_sRGB([+l / 100, +c, +h]).map(x => {
@@ -32,50 +47,86 @@ function LCH_to_sRGB_string(l, c, h, a = 100, forceInGamut = false) {
 	}).join(" ") + alpha_to_string(a) + ")";
 }
 
-function force_into_gamut(l, c, h, isLCH_within) {
-	// Moves an lch color into the sRGB gamut
-	// by holding the l and h steady,
-	// and adjusting the c via binary-search
-	// until the color is on the sRGB boundary.
-	if (isLCH_within(l, c, h)) {
+function force_into_gamut(rgb_space, l, c, h) {
+	// Algorithm adapted from the "color.js" `toGamut` method.
+	// (see https://github.com/LeaVerou/color.js/blob/master/src/color.js)
+	//
+	// Moves an OKLCH color into the sRGB gamut by holding the l and h steady,
+	// and adjusting the c via binary-search until the color is on the sRGB
+	// boundary.  At each step, the color estimate is channel-clipped and
+	// compared to the unclipped color using deltaEOK.  The clipped color is
+	// used if it is indistinguishable (deltaE < 0.02) from the unclipped color.
+	// This produces a higher-chroma mapping for certain problematic colors.
+	//
+	// See: https://www.w3.org/TR/css-color-4/#GM-chroma-local-MINDE
+
+	// scale lightness to [0, 1] range for OKLCH calculations
+	color = [+l / 100, +c, +h];
+
+	if (isLCH_within(rgb_space, color)) {
 		return [l, c, h];
 	}
 
-	let hiC = c;
-	let loC = 0;
-	const ε = .0001;
-	c /= 2;
+	const εE = 0.02;
+	let clipped = clip_OKLCH(rgb_space, color);
+	if (deltaEOKLCH(color, clipped) > εE) {
+		let hiC = c;
+		let loC = 0;
+		const ε = .0001;
+		c /= 2;
 
-	// .0001 chosen fairly arbitrarily as "close enough"
-	while (hiC - loC > ε) {
-		if (isLCH_within(l, c, h)) {
-			loC = c;
+		// .0001 chosen fairly arbitrarily as "close enough"
+		while (hiC - loC > ε) {
+			color[1] = c;  // update chroma
+			let clipped = clip_OKLCH(rgb_space, color);
+			let deltaE = deltaEOKLCH(color, clipped);
+			if (deltaE - εE < ε) {
+				loC = c;
+			} else {
+				hiC = c;
+			}
+			c = (hiC + loC)/2;
 		}
-		else {
-			hiC = c;
-		}
-		c = (hiC + loC)/2;
+
+		// final clip to clean up out-of-gamut approximations
+		color = clip_OKLCH(rgb_space, color);
+	} else {
+		color = clipped;
 	}
 
-	return [l, c, h];
+	// scale lightness back up to the picker range on return
+	color[0] *= 100;
+	return color;
+}
+
+function isLCH_within(rgb_space, lch) {
+	var rgb = rgb_space.from_lch(lch);
+	const ε = .000005;
+	return rgb.reduce((a, b) => a && b >= (0 - ε) && b <= (1 + ε), true);
 }
 
 function isLCH_within_sRGB(l, c, h) {
-	var rgb = OKLCH_to_sRGB([+l / 100, +c, +h]);
-	const ε = .000005;
-	return rgb.reduce((a, b) => a && b >= (0 - ε) && b <= (1 + ε), true);
+	return isLCH_within(sRGB_space, [+l / 100, +c, +h]);
 }
 
 function isLCH_within_P3(l, c, h) {
-	var rgb = OKLCH_to_P3([+l / 100, +c, +h]);
-	const ε = .000005;
-	return rgb.reduce((a, b) => a && b >= (0 - ε) && b <= (1 + ε), true);
+	return isLCH_within(P3_space, [+l / 100, +c, +h]);
 }
 
 function isLCH_within_r2020(l, c, h) {
-	var rgb = OKLCH_to_r2020([+l / 100, +c, +h]);
-	const ε = .000005;
-	return rgb.reduce((a, b) => a && b >= (0 - ε) && b <= (1 + ε), true);
+	return isLCH_within(r2020_space, [+l / 100, +c, +h]);
+}
+
+function deltaEOKLCH(reference, sample) {
+	// deltaEOK function from csswg.org samples expects OKLab colors
+	return deltaEOK(OKLCH_to_OKLab(reference), OKLCH_to_OKLab(sample));
+}
+
+function clip_OKLCH(rgb_space, lch) {
+	let clipped = rgb_space.from_lch(lch).map(
+		c => Math.max(0, Math.min(1, c))
+	);
+	return rgb_space.to_lch(clipped);
 }
 
 // Generate gradient stops for the sliders
